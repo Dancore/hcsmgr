dcd564314aa7a437bda13473af1ef7517eed6aea
Dan Kopparhed <dan.kopparhed@gmail.com>
2014-09-19 20:08:44 +0200

    Adding initial ldapjs based ldap client. However, current ldapjs version 0.7.1 lacks support for 'paged' responses, thus default server size limit of 1000 entries is a real problem. Paging support seems to be in the plan however. Also, for this implementation I may have a way to get around the problem, as long as there are never more than 1000 groups with more than 1000 members. OR admins have to increase server size limit.

---
+++ ldapclient.js
@@ -0,0 +1,74 @@
+var ldap = require('ldapjs');
+var assert = require('assert');
+var argv = process.argv;
+
+var server = "org.example.com"
+var port = 389
+var username = "user"
+var password = 'secret'
+var baseDN = "dc=example,dc=com"
+
+
+if(argv[2])
+  baseDN = "ou="+argv[2]+','+baseDN;
+
+var entries = 0
+
+var client = ldap.createClient({
+  url: 'ldap://'+server+':'+port
+});
+
+client.bind(username, password, function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('authenticated');
+  }
+});
+
+//client.unbind(function(err) {
+//});
+
+//  filter: '(&(l=Seattle)(email=*@foo.com))',
+var options = {
+  scope: 'sub',
+  sizeLimit: 110,
+  paged: {
+    pageSize: 100,
+    pagePause: true
+  }
+};
+
+//client.search(baseDN, function(err, res) {
+client.search(baseDN, options, function(err, res) {
+  assert.ifError(err);
+
+  res.on('searchEntry', function(entry) {
+	entries++;
+//    console.log('entry: ' + JSON.stringify(entry.object));
+	console.log('entry '+entries+': '+entry.dn+' ')
+	entry.json.attributes.forEach(function(attr) {
+	  if(attr.type === "description")
+		return console.log('Description: '+ attr.vals[0])
+
+	});
+  });
+  res.on('searchReference', function(referral) {
+    console.log('referral: ' + referral.uris.join());
+  });
+  res.on('error', function(err) {
+    console.error('error: ' + err.message);
+  });
+  res.on('end', function(result) {
+    console.log('status: ' + result.status);
+    console.log('Found ' + entries +' entries');
+  });
+
+  res.on('page', function (res, cb) {
+    // call 'cb' when processing complete for a page
+//    asyncWaitForProcessing(cb);
+    console.log('status: ' + result.status);
+  });
+
+});
+
